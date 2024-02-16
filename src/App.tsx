bea63eb4e558e06c211e635ddf51dadfe5eff46e
LClarkson <l.j.clarkson@gmail.com>
2024-02-15 17:22:08 -0800

    add CSS media query for screen size, only display remote control component on screens < 600px

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,21 @@ import Remote from './components/remote.tsx';
 
 
 function App() {
+  /************************************** STATE INIT **************************************/
 
-/************************************** STATE INIT **************************************/
-  
-  const [videoID, setVideoID] = useState('l7rce6IQDWs')
+  const [videoID, setVideoID] = useState("l7rce6IQDWs");
 
-/************************************* CHANGE CHANNEL ***********************************/
+  /************************************* CHANGE CHANNEL ***********************************/
 
-const videos = [
-  'l7rce6IQDWs',
-  '8v0QwgWPqPU',
-  'he-7vs0BkLE',
-  'icwYkOun_Po',
-]
+  const videos = ["l7rce6IQDWs", "8v0QwgWPqPU", "he-7vs0BkLE", "icwYkOun_Po"];
 
-const changeChannel = () => {
-  const randomVideo = Math.floor(Math.random() * videos.length);
-  setVideoID(videos[randomVideo]);
-}
+  const changeChannel = () => {
+    const randomVideo = Math.floor(Math.random() * videos.length);
+    setVideoID(videos[randomVideo]);
+  };
 
-/****************************************** HTML ****************************************/
-  
+  /****************************************** HTML ****************************************/
+
   return (
     <>
       <div id="viewPort">
@@ -35,9 +29,11 @@ const changeChannel = () => {
         <YoutubePlayer videoID={videoID} videoTime={50} />
       </div>
       <TimeBar />
-      <Remote changeChannel={changeChannel} />
+      <div id="remote">
+        <Remote changeChannel={changeChannel} />
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
