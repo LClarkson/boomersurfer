7931894ed9cf98de271ae19c9c37f72aa2bf4aa1
LClarkson <l.j.clarkson@gmail.com>
2024-02-20 11:33:11 -0800

    add react query

--- client/src/components/Guide.tsx
+++ client/src/components/Guide.tsx
@@ -1,6 +1,42 @@
+import { useState, useEffect } from 'react';
 import '../styles/Guide.css';
 
 const Guide = () => {
+
+  const [shows, setShows] = useState<Video[]>([]);
+  interface Video {
+    videoID: string;
+    channelID: string;
+    playlistID: string;
+    videoURL: string;
+    videoTitle: string;
+    videoDescription: string;
+    videoUploadDate: string;
+   }
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch("/watch");
+        if (!response.ok) {
+          throw new Error("Failed to fetch videos");
+        }
+        const videosData: Video[] = await response.json();
+        setShows(videosData);
+        console.log(videosData);
+              
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error("Error fetching videos:", (error as Error).message);
+        } else {
+          console.error("Unexpected error:", error);
+        }
+      }
+    };
+
+    fetchVideos();
+  }, []);
+
     return (
       <>
         <div className="ChannelScroller">
