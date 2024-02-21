82a9a50472df754edf8a76361289ad5df8cad1ef
LClarkson <l.j.clarkson@gmail.com>
2024-02-20 17:29:02 -0800

    modify guide style

--- client/src/components/Guide.tsx
+++ client/src/components/Guide.tsx
@@ -14,6 +14,8 @@ const Guide = () => {
     videoUploadDate: string;
   }
 
+/************************************** FETCH GUIDE **************************************/
+
   const { isPending, error, data } = useQuery({
     queryKey: ["shows"],
     queryFn: () => fetch("/watch").then((res) => res.json()),
@@ -22,6 +24,7 @@ const Guide = () => {
   if (isPending) return "Loading...";
   if (error) return "An error has occurred: " + error.message;
 
+  // create divs for each show
   const shows = data.map((show: Video) => (
     <div key={show.videoID} className="Show">
       <h3>{show.videoTitle}</h3>
@@ -31,6 +34,16 @@ const Guide = () => {
 
   return (
     <>
+    <div className="ChannelList">
+      <div className="Channel"> <h3>24 FIRST THINGS FIRST</h3> </div>
+      <div className="Channel"> <h3>25 FLAT EARTH DAVE</h3></div>
+      <div className="Channel"> <h3>26 BEN EATER</h3></div>
+      <div className="Channel"> <h3>27 NASA</h3></div>
+    </div>
+    
+      <div className="ChannelScroller">{shows}</div>
+      <div className="ChannelScroller">{shows}</div>
+      <div className="ChannelScroller">{shows}</div>
       <div className="ChannelScroller">{shows}</div>
     </>
   );
